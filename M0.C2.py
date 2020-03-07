f4b016fc899bece8e6e75f86fb294cb3de5ebe34
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2020-03-07 03:44:10 -0700

    First commit

---
+++ M0.C2.py
@@ -0,0 +1,25 @@
+# Script will start by asking for name, greeting user, and then
+# listing off locations. Output and next steps depend on user's
+# input. See https://github.com/Static-Void-Academy/Module0-Challenges/blob/master/prompts/M0.C1.md
+
+print('Hello! What is your name?')
+name = input()
+
+print(f'Welcome to SVA, {name}! Where would you like to go?')
+print('Choose from: Cafeteria, Office, Playground, Restroom')
+location = input()
+
+if location == 'Cafeteria':
+    print('The Cafeteria is straight ahead at the end. Enjoy!')
+elif location == 'Office':
+    print('The Office is up the stairs and to the left.')
+elif location == 'Playground':
+    print('Please confirm your age:')
+    age = int(input())
+    if age <= 11:
+        print('Our Playground is back out the main door and around the side.')
+    else:
+        print('Sorry, you are too old!')
+elif location == 'Restroom':
+    print('The Restroom is down the hall and to the right before the cafeteria.')
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Module 0 Solutions
+
+This repository houses the solutions to Module 0's challenge problems.
+
+Prompt             | Solution                        | Description                                                         | Topics Tested
+-------------------|---------------------------------|---------------------------------------------------------------------|-------------------------
+[M0.C1](https://github.com/Static-Void-Academy/Module0-Challenges/M0.C1.md) | [M0.C1.py](/M0.C1.py) | The SVA Charity needs your help to write a program to greet guests. | <ul><li>Taking input</li><li>Conditionals (If/Elif/Else)</li><li>Nesting and scope</li><li>Type conversion</li></ul>
+[M0.C2](https://github.com/Static-Void-Academy/Module0-Challenges/M0.C2.md) | [M0.C2.py](/M0.C2.py) | You're trying to be smarter about your work at a sign company.      | <ul><li>Taking input</li><li>Looping</li><li>Conditionals</li></ul>
+
